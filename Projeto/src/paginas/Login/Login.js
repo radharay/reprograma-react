ac2209f4936c249fe21985b8ff2c809cab3c549d
Radharay <radharayg@gmail.com>
2018-10-30 16:16:42 -0300

    aula05(conceito): ajustada regex de email

--- Projeto/src/paginas/Login/Login.js
+++ Projeto/src/paginas/Login/Login.js
@@ -13,7 +13,7 @@ function Login() {
       
       <Legenda htmlFor="email">Email:</Legenda>
       <Campo id="email"type="email"name="email"placeholder="Email" required
-      minLength={10}/>
+      pattern={/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/}/>
       
       <Legenda htmlFor="senha">Senha:</Legenda>
       <Campo id="senha"type="password"name="senha"placeholder="Senha"required
