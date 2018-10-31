f498e0af410487d085f6d3ef3b9eaa6caf4afae2
radharay <radharayg@gmail.com>
2018-10-30 21:20:44 -0300

    aula 06 - Conta.js

--- Projeto/src/paginas/Conta/Conta.js
+++ Projeto/src/paginas/Conta/Conta.js
@@ -1,36 +1,67 @@
 
-import React from 'react'
+import React, { Component } from 'react'
 import Link from '../../componentes/Link/Link'
 import Botao from '../../componentes/Botao/Botao'
 import Legenda from '../../componentes/Legenda/Legenda'
 import Campo from '../../componentes/Campo/Campo'
 import './Conta.css'
 
-function Conta() {
-  return (
-    <main className="conta">
-      <h1>Conta</h1>
-      <p>Envie o formulário para criar uma conta</p>
-      
-      <Legenda htmlFor="nome">Nome:</Legenda>
-      <Campo id="nome"type="nome"name="nome"placeholder="Nome"  required
-      minLength={10}/>
 
-      <Legenda htmlFor="telefone">Telefone:</Legenda>
-      <Campo id="telefone"type="tel"name="telefone"placeholder="Telefone" required/>
+class Conta extends Component{
+  constructor(props){
+    super(props)
+    this.nomeRef = React.createRef()
+    this.telefoneRef = React.createRef()
+    this.emailRef = React.createRef()
+    this.senhaRef = React.createRef()
+    this.state = { desabilitado: true }
 
-      <Legenda htmlFor="email">Email:</Legenda>
-      <Campo id="email"type="email"name="email"placeholder="Email" required />
-      
-      <Legenda htmlFor="senha">Senha:</Legenda>
-      <Campo id="senha"type="password"name="senha"placeholder="Senha"  required
-      minLength={6}/>
-      
-      <Botao>Enviar</Botao>
+  }
 
-      <Link url="/login">Fazer login</Link>
-    </main>
-  )
+  habilitaOuDesabilita = () => {
+    const campoEmail = this.emailRef.current
+    const campoSenha = this.senhaRef.current
+    const campoNome = this.nomeRef.current
+    const campoTelefone = this.telefoneRef.current
+
+    if (campoEmail.temErro() || campoSenha.temErro() || campoNome.temErro() || campoTelefone.temErro()){
+      this.setState({ desabilitado: true })
+    } else {
+      this.setState({ desabilitado: false })
+    }
+  }
+
+  render(){
+    return (
+      <main className="conta">
+        <h1>Conta</h1>
+        <p>Envie o formulário para criar uma conta</p>
+        
+        <Legenda htmlFor="nome">Nome:</Legenda>
+        <Campo ref={this.nomeRef} id="nome"type="nome"name="nome"placeholder="Nome"  required
+        minLength={10} onChange={this.habilitaOuDesabilita} />
+  
+        <Legenda htmlFor="telefone">Telefone:</Legenda>
+        <Campo ref={this.telefoneRef} id="telefone"type="tel"name="telefone"placeholder="Telefone" required onChange={this.habilitaOuDesabilita}/>
+  
+        <Legenda htmlFor="email">Email:</Legenda>
+        <Campo ref={this.emailRef} id="email"type="email"name="email"placeholder="Email" required onChange={this.habilitaOuDesabilita}/>
+        
+        <Legenda htmlFor="senha">Senha:</Legenda>
+        <Campo ref={this.senhaRef} id="senha"type="password"name="senha"placeholder="Senha"  required
+        minLength={6} onChange={this.habilitaOuDesabilita}/>
+        
+        <Botao desabilitado={this.state.desabilitado}>Enviar</Botao>
+  
+        <Link url="/login">Fazer login</Link>
+      </main>
+    )
+  }
 }
 
 export default Conta
+
+
+
+
+
