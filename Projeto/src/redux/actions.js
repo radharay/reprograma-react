ca664044431ecf892e123add330122684d2ecd73
Radharay <radharayg@gmail.com>
2018-11-12 14:58:08 -0200

    aula(redux): concluido componente postit

--- Projeto/src/redux/actions.js
+++ Projeto/src/redux/actions.js
@@ -1,18 +1,33 @@
 export function logaUsuario(dados) {
-    return {
-      type: 'LOGA_USUARIO',
-      dados
-    }
+  return {
+    type: 'LOGA_USUARIO',
+    dados
   }
-   export function deslogaUsuario() {
-    return {
-      type: 'DESLOGA_USUARIO'
-    }
-  } 
+}
 
-  export function cadastraPostit(dados) {
-    return {
-      type: 'CADASTRA_POSTIT',
-      dados
-    }
+export function deslogaUsuario() {
+  return {
+    type: 'DESLOGA_USUARIO'
   }
+}
+
+export function cadastraPostit(dados) {
+  return {
+    type: 'CADASTRA_POSTIT',
+    dados
+  }
+}
+
+export function alteraPostit(dados) {
+  return {
+    type: 'ALTERA_POSTIT',
+    dados
+  }
+}
+
+export function removePostit(id) {
+  return {
+    type: 'REMOVE_POSTIT',
+    id
+  }
+}
--- Projeto/src/redux/reducers.js
+++ Projeto/src/redux/reducers.js
@@ -1,40 +1,45 @@
-import { combineReducers} from 'redux'
+import { combineReducers } from 'redux'
 
 let usuarioInicial = null
 
 const json = localStorage.getItem('usuario')
 if (json) {
-  usuarioInicial = JSON.parse(json)
+    usuarioInicial = JSON.parse(json)
 }
 
-function usuario(state = usuarioInicial, action){
-    switch(action.type){
+function usuario(state = usuarioInicial, action) {
+    switch (action.type) {
         case 'LOGA_USUARIO':
-        const usuarioLogado = action.dados
-        const json = JSON.stringify(usuarioLogado)
-      localStorage.setItem('usuario', json)
-        return usuarioLogado
-        localStorage.removeItem('usuario')
+            const usuarioLogado = action.dados
+            const json = JSON.stringify(usuarioLogado)
+            localStorage.setItem('usuario', json)
+            return usuarioLogado
         case 'DESLOGA_USUARIO':
-        const usuarioDeslogado = null
-        return usuarioDeslogado
+            localStorage.removeItem('usuario')
+            const usuarioDeslogado = null
+            return usuarioDeslogado
         default:
-        return state
+            return state
     }
 }
 
-function postits(state = [], action){ // array vazia
-    switch(action.type){
-        default: 
-        return state
+function postits(state = [], action) {
+    switch (action.type) {
+        case 'CADASTRA_POSTIT':
+            return state.concat(action.dados)
+        case 'ALTERA_POSTIT':
+            return state.map(item =>
+                item.id === action.dados.id ? action.dados : item
+            )
+        case 'REMOVE_POSTIT':
+            return state.filter(item => item.id !== action.id)
+        default:
+            return state
     }
 }
-
-// agora temos dois redutores, usuario e postits
-//combinar os dois
 
 const reducers = combineReducers({
-    usuario: usuario, // propriedade:valor
+    usuario: usuario,
     postits: postits
 })
 
